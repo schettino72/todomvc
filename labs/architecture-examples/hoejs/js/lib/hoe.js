a3e2f9b2388bca4bdb302adffa542c4a82fab27a
schettino72 <schettino72@gmail.com>
2013-01-12 04:45:45 +0800

    update hoe.js

--- labs/architecture-examples/hoejs/js/lib/hoe.js
+++ labs/architecture-examples/hoejs/js/lib/hoe.js
@@ -1,13 +1,31 @@
-// create new object and apply arguments
-// hoe(tag [, param ...]
+/** @license
+ *   hoe.js - [http://schettino72.github.com/hoe.js]
+ *   MIT license - Copyright (c) 2012 Eduardo Naufel Schettino
+ */
+
+/**
+ * @namespace creates new DOM elements
+ * @param {String} tag name of tag to be created
+ * @param [param[]] can take any number of params, {@link hoe.jquery_plugin}
+ * @returns {jQuery}
+ */
 var hoe = function(tag){
     var $ele = jQuery(document.createElement(tag));
     return $ele.hoe.apply($ele, Array.prototype.slice.call(arguments, 1));
 };
 
+/**
+ * Exposed to any jQuery object as 'hoe' plugin.
+ * Guess which jQuery method should be applied to object,
+ * operation depends on param type.
+ * @param {String} [param] append as text to element content
+ * @param {Object} [param] set as html element attributes
+ * @param {DOMElement[]|jQuery[]} [param] append param into element content
+ * @returns {jQuery}
+ */
 hoe.jquery_plugin = function() {
     /*
-      Guess which jQuery method should be applied to arguments based on type:
+       to arguments based on type:
        - string: append text
        - plain object: set html attributes
        - DOM element & jQuery object: append elements
@@ -30,11 +48,8 @@ hoe.jquery_plugin = function() {
                     $ele.attr(name, param[name]);
                 }
             }
-            else if((param instanceof jQuery) || (param instanceof Element)){
-                $ele.append(param);
-            }
             else {
-                throw Error("Invalid object: " + param);
+                $ele.append(param);
             }
         }
         else {
@@ -50,32 +65,59 @@ hoe.jquery_plugin = function() {
 
 jQuery.fn.hoe = hoe.jquery_plugin;
 
-// build functions to create hoe objcts on namespace
-// by default add most common tags to window object
+/**
+ * Similar to `hoe()` but instead of returning an element returns
+ * a function that creates new elements including the parameters
+ * passed to partial
+ * @param {String} tag name of tag to be created
+ * @param [param[]] can take any number of params, {@link hoe.jquery_plugin}
+ * @returns function
+ */
+hoe.partial = function(tag){
+    var partial_args = Array.prototype.slice.call(arguments, 1);
+    return function(){
+        var $ele = jQuery(document.createElement(tag));
+        $ele.hoe.apply($ele, partial_args);
+        return $ele.hoe.apply($ele, arguments);
+    };
+};
+
+
+/**
+ * build functions to create hoe objcts on namespace
+ * by default add most common tags to window object
+ * @constant
+ */
 hoe.init_default_tags = [
     'body', 'div','span', 'pre', 'p', 'a', 'ul', 'ol', 'li',
     'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong',
-    'section', 'header', 'footer',
+    'section', 'header', 'footer', 'br',
     'form', 'label', 'input', 'select', 'button',
     'table', 'thead', 'tbody', 'tfoot', 'tr', 'th','td'
 ];
+
+
+/**
+ * define factory functions to create DOM elements
+ * @param {Object} [namespace=window] namespace where functions will be created
+ * @param {String[]} [tags=hoe.init_default_tags] functions will be created
+          for given tags
+ */
 hoe.init = function(namespace, tags){
     namespace = namespace || window;
     tags = tags || hoe.init_default_tags;
-
-    function _create_function(tag){
-        return function(){
-            var $ele = jQuery(document.createElement(tag));
-            return $ele.hoe.apply($ele, arguments);
-        };
-    }
     for (var i=0, max=tags.length; i<max; i++){
-        namespace[tags[i]] = _create_function(tags[i]);
+        namespace[tags[i]] = hoe.partial(tags[i]);
     }
 };
 
 
-
+/**
+ * create new type by inheriting from other types
+ * @param {Object} base_type Type to inherit from
+ * @param {Function} [constructor] over-write constructor from base_type
+ * @return {Type} new Type/constructor
+ */
 hoe.inherit = function (base_type, constructor){
     var new_type;
     if (constructor){
@@ -89,54 +131,105 @@ hoe.inherit = function (base_type, constructor){
     return new_type;
 };
 
-
-// object helpers that bind scope to object
+/**
+ * Base Type with helpers that bind scope to object.
+ * Constructor can be used to create new types.
+ * @constructor
+ * @param {Function} constructor constructor of a new Type
+ * @returns {Function/Type} new Type that inherit from hoe.Type
+ */
 hoe.Type = function(constructor){
     return hoe.inherit(hoe.Type, constructor);
 };
-hoe.Type.prototype = {
-    // event system
-    on: function(observed, event, callback){
-        if(observed instanceof jQuery){
-            observed.bind(event, $.proxy(callback, this));
-        }
-        else{
-            if (typeof observed._hoe_obs === 'undefined'){
-                observed._hoe_obs = {};
-            }
-            if (!(event in observed._hoe_obs)){
-                observed._hoe_obs[event] = [];
-            }
-            observed._hoe_obs[event].push({scope:this, fn:callback});
-        }
-    },
-    trigger: function(event){
-        if (this._hoe_obs && this._hoe_obs[event]){
-            var callbacks = this._hoe_obs[event];
-            for (var i=0, max=callbacks.length; i<max; i++){
-                callbacks[i].fn.apply(callbacks[i].scope,
-                                      Array.prototype.slice.call(arguments, 1));
-            }
-        }
-    },
 
-    // functional stuff
-    forEach: function(seq, fn){
-        if (typeof seq == 'array'){
-            for(var i = 0, len = seq.length; i < len; ++i) {
-                fn.call(this, seq[i], i, seq);
-            }
+
+/**
+ * Attach a callback on this object scope for event from observed
+ * @param {jQuery|hoe.Type} observed object that trigger events
+ * @param {String} event name of the event
+ * @param {Function} callback to be executed when event is triggered.
+ *                      this will be bound to current object.
+ */
+hoe.Type.prototype.on = function(observed, event, callback){
+    if(observed instanceof jQuery){
+        observed.bind(event, $.proxy(callback, this));
+    }
+    else{
+        if (typeof observed._hoe_obs === 'undefined'){
+            observed._hoe_obs = {};
         }
-        else if (typeof seq == 'object'){
-            for (var key in seq){
-                fn.call(this, seq[key], key, seq);
-            }
+        if (!(event in observed._hoe_obs)){
+            observed._hoe_obs[event] = [];
+        }
+        observed._hoe_obs[event].push({scope:this, fn:callback});
+    }
+};
+
+/**
+ * Trigger an event for this object
+ * @param {String} event name of the event
+ * @param [arguments] other arguments will be passed to the callback
+ */
+hoe.Type.prototype.trigger = function(event){
+    if (this._hoe_obs && this._hoe_obs[event]){
+        var callbacks = this._hoe_obs[event];
+        for (var i=0, max=callbacks.length; i<max; i++){
+            callbacks[i].fn.apply(callbacks[i].scope,
+                                  Array.prototype.slice.call(arguments, 1));
         }
     }
 };
 
+/**
+ * Iterate through Arrays or plain objects keeping the scope
+ * @param {Array|Object} seq contains items that will be iterated
+ * @param {Function} callback to be called for each item.
+          The callback the 3 paramaters:
+          1) value
+          2) index/key
+          3) reference to the whole sequence
+ */
+hoe.Type.prototype.forEach = function(seq, fn){
+    if (jQuery.type(seq) == 'array'){
+        for(var i = 0, len = seq.length; i < len; ++i) {
+            fn.call(this, seq[i], i, seq);
+        }
+    }
+    else { // must be an object
+        for (var key in seq){
+            fn.call(this, seq[key], key, seq);
+        }
+    }
+};
 
-// node stuff
+/**
+ * Create an Array iterating through Arrays or plain objects keeping the scope
+ * @param {Array|Object} seq contains items that will be iterated
+ * @param {Function} callback to be called for each item.
+          The callback return value will be inserted in the returned Array
+          The callback the 3 paramaters:
+          1) value
+          2) index/key
+          3) reference to the whole sequence
+ * @return Array
+ */
+hoe.Type.prototype.map = function(seq, fn){
+    var result = [];
+    if (jQuery.type(seq) == 'array'){
+        for(var i = 0, len = seq.length; i < len; ++i) {
+            result.push(fn.call(this, seq[i], i, seq));
+        }
+    }
+    else { // must be an object
+        for (var key in seq){
+            result.push(fn.call(this, seq[key], key, seq));
+        }
+    }
+    return result;
+};
+
+
+/** @exports */
 if (typeof exports !== 'undefined'){
     exports.hoe = hoe;
 }
